98ce87d0ee03e0b0db45be2ff68540dec6a3b571
Paolo <paolo.alhambra@yahoo.com>
2023-02-27 22:29:14 -0800

    Started question section

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,11 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What do you want in your description?', ' Table of contents?', 'What are the instructions to install?', ' What is this project used for?', 'What license are you using?',
+  'Who contributed?', 'What kind of tests did you perform?', 'How do I contact you for additional questions'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
